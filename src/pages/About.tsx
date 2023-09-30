46d6a80824ad03083fd1cd55e91335d12c1338fd
Chizi Victor <chiziwokoma@gmail.com>
2023-09-30 01:07:18 +0100

    fix: typos in about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,3 @@
-// import { useAnimation, motion } from 'framer-motion';
 import { BsInstagram, BsTwitter, BsWhatsapp } from 'solid-icons/bs';
 import { imageKitURI } from '../lib/constants';
 import IntroLoader from '../components/IntroLoader';
@@ -63,25 +62,25 @@ const About = () => {
               in Port Harcourt city in River State. My first experience as an
               artist was when I began tracing and making sketches at 5years old.
               My older brother loved drawing and I always watched him play
-              around his pencils as it mademewant to draw as well; He was my
+              around his pencils as it made me want to draw as well; He was my
               first influence.
               <br />
               Being among other kids who had the same burning desire and
-              enthusiasmserved as another form of inspiration but the best of
-              them all was when I was on my way home in 2016, I sawaroadside
+              enthusiasm served as another form of inspiration but the best of
+              them all was when I was on my way home in 2016, I saw a road side
               artist working on a realistic drawing. I had never seen a drawing
-              as good and real ashis and it was there I realise that I had to
+              as good and real as his and it was there I realise that I had to
               improve and expand my knowledge in Art.
               <br />
               I believe that being an artist something that one is born to do
               and then followed up with consistent training as I studied through
-              practical experiences, learning through trial anderror and
+              practical experiences, learning through trial and error and
               studying the works of Kelvin Okafor and many other artists.it was
-              here I further expandedmy knowledge on graphite, and charcoal
+              here I further expanded my knowledge on graphite, and charcoal
               drawings.
               <br />
               <br />I have been drawing profressionally for 6 years and I have
-              created a lot of art pieces andalsomade numerous commissioned
+              created a lot of art pieces and also made numerous commissioned
               drawings as well. I work primarily using graphite and charcoal
               pencils and I currently work from home. My biggest Achievement
               would be inspiring other artist, friends and children in learning
@@ -91,7 +90,7 @@ const About = () => {
         </div>
 
         <nav class="about__nav">
-          <div class="about__nav-links transition-all">
+          <div class="about__nav-links">
             <a
               href="https://twitter.com/elisha_tams"
               rel="noreferrer"
@@ -123,7 +122,7 @@ const About = () => {
               href="https://www.chizi.dev"
               rel="noreferrer"
               target="_blank"
-              class="interactable link text-gray-500"
+              class="interactable link"
             >
               Chizi Victor
             </a>
